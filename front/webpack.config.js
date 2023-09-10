b4cb1d120319895f07ea16f54f1956456e346405
Paul Louvel <plouvel@student.42.fr>
2023-09-10 17:24:32 +0200

    front uses typescript

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -1,9 +1,22 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
   },
 };
