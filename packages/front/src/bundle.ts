58b46228e013695b2b243bd57480435bd840043a
Paul Louvel <plouvel@student.42.fr>
2023-09-15 14:53:10 +0200

    feat: relocated front

--- packages/front/src/bundle.ts
+++ packages/front/src/bundle.ts
@@ -1,10 +1,17 @@
 import _ from 'lodash';
+import './style.css';
+import printMe from './print';
 
 function component() {
   const element = document.createElement('div');
+  const button = document.createElement('button');
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  button.innerHTML = 'Bonjour cliquer moi';
+  button.onclick = printMe;
+
+  element.appendChild(button);
 
   return element;
 }
---
+++ packages/front/webpack.config.js
@@ -0,0 +1,37 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+export default {
+  entry: { index: './src/bundle.ts', print: './src/print.ts' },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ title: 'Output Management' })],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.tsx'],
+  },
+};
